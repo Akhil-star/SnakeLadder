1b15a431d3ceb2f1a3f66c3904075ddfa271f197
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 12:26:16 +0530

    Checks Options

--- src/main/java/com/snakeladder/App.java
+++ src/main/java/com/snakeladder/App.java
@@ -6,9 +6,41 @@ package com.snakeladder;
  */
 public class App 
 {
+    public static final int No_Play=0;
+    public static final int Snake=1;
+	public static final int Laddder =2 ;
+
+    static int Position1=0;
+
     public static void main( String[] args )
     {
-    	 int Die_Num=(int) ((Math.floor(Math.random()*10)%6)+1);
-         System.out.println("Die Number : "+Die_Num);        
+  	     int Die_Num=(int) ((Math.floor(Math.random()*10)%6)+1);
+         int Options = (int) Math.floor(Math.random()*10)%3;
+         
+         System.out.println("Die Number :" +Die_Num);
+         System.out.println("Check_Option :"+Options);
+         
+         switch(Options) {
+         case Laddder :    	  
+         {
+	        if(Position1 <=100 && (Position1+Die_Num) <=100 )
+	        Position1 = Position1 + Die_Num; 
+	        break;
+	  
+         }
+         case Snake :
+         {  
+	        if(Position1 >0 && (Position1-Die_Num)>0 )
+	        Position1=Position1-Die_Num;
+	        break;
+         } 
+        case No_Play :
+         {
+            Position1=Position1;
+            break;
+         }
+     }
+   	     System.out.println("Position of Ladder : "+Position1);
+
     }
 }
