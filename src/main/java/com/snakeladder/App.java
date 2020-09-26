145fe960594d6874cb0f170b5378b29893aeb649
Akhil-star <akhilakki1305@gmail.com>
2020-09-26 12:00:46 +0530

    Position

---
+++ src/main/java/com/snakeladder/App.java
@@ -0,0 +1,15 @@
+package com.snakeladder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static int Position1=0;
+
+    public static void main( String[] args )
+    {
+        System.out.println("Position of Player :"+Position1);
+    }
+}
